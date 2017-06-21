0f24460ff1f58f87fad70215b57467c87166e59b
ramkrishan-sahu <ram04190@gmail.com>
2017-06-21 14:10:12 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
+#Create a one-dimensional array-like object containing an array of data
+# https://app.commit.live/lesson/fsdse-python-assignment-87
+import numpy as np
 import pandas as pd
 
+Lis = [2, 4, 6, 8, 10]
 
-def solution(array):
-    """
-    Enter your code here
-    """
+def solution(li):
+    fin = pd.Series(li)
+    print fin
+    type (fin)
+    return fin
+
+solution(Lis)
